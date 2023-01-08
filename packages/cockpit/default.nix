93f4db8f0ede20423f47623889d037af3791fbf7
Andreas Mårtensson <andreas@addem.se>
2023-01-08 20:10:15 +0100

    this was in my stash

--- packages/cockpit/default.nix
+++ packages/cockpit/default.nix
@@ -1,78 +1,62 @@
-{ stdenv
-, lib
-, fetchgit
+{ lib
+, stdenv
 , fetchzip
-, callPackage
-, makeWrapper
-, automake
-, autoreconfHook
-, git
-, nodejs
 , pkgconfig
-, python3
-, coreutils
-, gnused
+, glib
 , systemd
-, openssl
-, openssh
-, dbus
-, keyutils
-, polkit
-, gtk-doc
 , json-glib
+, gnutls
 , krb5
-, nfs-utils
-, pam_krb5
-, linux-pam
+, polkit
+, libssh
 , pam
 , libxslt
-, libssh
-, intltool
-, gobject-introspection
-, networkmanager
 , xmlto
-, nodePackages
-, perlPackages
+, python3
+, gnused
+, coreutils
+, makeWrapper
+, openssl
 , extraPackages ? [ ]
 }:
+
+let
+  path = lib.makeSearchPath "bin" ([ "$out" "/run/wrappers" "/run/current-system/sw" ] ++ extraPackages);
+in
 stdenv.mkDerivation rec {
   pname = "cockpit";
-  version = "265";
+  version = "267";
 
   src = fetchzip {
     url = "https://github.com/cockpit-project/cockpit/releases/download/${version}/cockpit-${version}.tar.xz";
-    sha256 = "+eAN6aNLnGVHLnku7PfqyVd3iCIatN+BJgS3bWDfbZg=";
+    sha256 = "0bdc2qzqcz4k92asxip00nambn47a4w12c4vl08xj6nc1ja627ig";
   };
 
+  configureFlags = [
+    "--disable-doc"
+    "--with-systemdunitdir=$(out)/lib/systemd/system"
+    "--sysconfdir=/etc"
+  ]
+  ++ (if lib.any ({ name, ... }: name == "pcp") extraPackages then [ ] else [ "--disable-pcp" ]);
+
   nativeBuildInputs = [
-    automake
-    git
-    autoreconfHook
     pkgconfig
     python3
+    gnused
     makeWrapper
   ];
 
   buildInputs = [
+    glib
     systemd
-    dbus
-    keyutils
-    polkit
-    gtk-doc
     json-glib
+    gnutls
     krb5
-    nfs-utils
-    pam_krb5
-    linux-pam
-    libxslt
+    polkit
     libssh
-    intltool
-    gobject-introspection
-    networkmanager
+    pam
+    libxslt
     xmlto
-    perlPackages.JavaScriptMinifierXS
-    perlPackages.FileShareDir
-    perlPackages.JSON
   ] ++ extraPackages;
 
   patches = [
@@ -116,19 +100,25 @@ stdenv.mkDerivation rec {
      make install
   '';
 
+  postPatch = ''
+    patchShebangs tools
+    sed -r '/^cmd_make_package_lock_json\b/ a exit 0' -i tools/node-modules
+    substituteInPlace Makefile.in \
+      --replace "\$(DESTDIR)\$(sysconfdir)" "$out/etc"
+    substituteInPlace src/session/session-utils.h \
+      --replace "DEFAULT_PATH \"" "DEFAULT_PATH \"${path}:"
+  '';
+
   postInstall = ''
-    wrapProgram "$out/libexec/cockpit-certificate-helper" --suffix PATH : "${lib.makeBinPath [coreutils openssl]}"
+    wrapProgram "$out/libexec/cockpit-certificate-helper" \
+      --suffix PATH : "${lib.makeBinPath [ coreutils openssl ]}"
   '';
 
   meta = with lib; {
-    description = "A sysadmin login session in a web browser";
-    homepage = "https://www.cockpit-project.org";
-    license = licenses.lgpl2;
-    platforms = [ "x86_64-linux" ];
-    maintainers = with maintainers;
-      [
-        thefenriswolf
-        baronleonardo
-      ];
+    description = "Web-based graphical interface for servers";
+    license = licenses.lgpl21;
+    homepage = "https://cockpit-project.org/";
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ ];
   };
 }
