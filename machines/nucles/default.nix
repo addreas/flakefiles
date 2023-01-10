8cbc287ff5291c2e9cb41c32437705ac293e00d1
Andreas Mårtensson <andreas@addem.se>
2023-01-10 22:50:45 +0100

    anyInterface

--- machines/nucles/default.nix
+++ machines/nucles/default.nix
@@ -24,6 +24,7 @@
   systemd.network.enable = true;
   systemd.network.networks.lan.name = "en*";
   systemd.network.networks.lan.DHCP = "yes";
+  systemd.network.wait-online.anyInterface = true;
   services.resolved.dnssec = "false"; # dnssec fails for localdomain and breaks stuff
 
   security.sudo.wheelNeedsPassword = false;
