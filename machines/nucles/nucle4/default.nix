d8789d5f95f4a20be19e083d8a314578d3e23664
Andreas Mårtensson <andreas@addem.se>
2023-01-10 21:23:31 +0100

    nucle4 kubeadm bootstrap

--- machines/nucles/nucle4/default.nix
+++ machines/nucles/nucle4/default.nix
@@ -6,5 +6,10 @@
   ];
 
   networking.hostName = "nucle4";
+
+  services.kubeadm.init = {
+    enable = true;
+    bootstrapTokenFile = "/var/secret/kubeadm-bootstrap-token"; # ssh nucle1.localdomain -- kubeadm token create | ssh nucle4.localdomain -- sudo tee /var/secret/kubeadm-bootstrap-token
+  };
 }
 
