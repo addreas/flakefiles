f2ee4f86cfd7ac2b8b1502cfd693b26a35b43b8c
Andreas Mårtensson <andreas@addem.se>
2023-02-11 16:00:09 +0100

    disable nut

--- machines/sergio/monitoring.nix
+++ machines/sergio/monitoring.nix
@@ -7,26 +7,26 @@
   systemd.services."prometheus-smartctl-exporter".serviceConfig.DeviceAllow =
     [ "block-blkext rw" "block-sd rw" "char-nvme rw" ];
 
-  power.ups.enable = true;
-  power.ups.mode = "netserver";
-  power.ups.ups.ups = {
-    port = "auto";
-    driver = "usbhid-ups";
-  };
+  # power.ups.enable = true;
+  # power.ups.mode = "netserver";
+  # power.ups.ups.ups = {
+  #   port = "auto";
+  #   driver = "usbhid-ups";
+  # };
 
-  environment.etc = {
-    "nut/upsd.conf".text = "LISTEN 0.0.0.0";
-    "nut/upsd.users".text = ''
-      [monuser]
-      upsmon master
-      password = "monuser"
-    '';
-    "nut/upsmon.conf".text = ''
-      MONITOR ups@localhost 1 monuser "monuser" master
-    '';
-  };
+  # environment.etc = {
+  #   "nut/upsd.conf".text = "LISTEN 0.0.0.0";
+  #   "nut/upsd.users".text = ''
+  #     [monuser]
+  #     upsmon master
+  #     password = "monuser"
+  #   '';
+  #   "nut/upsmon.conf".text = ''
+  #     MONITOR ups@localhost 1 monuser "monuser" master
+  #   '';
+  # };
 
-  system.activationScripts.var-lib-nut = "mkdir -p /var/lib/nut; chmod o-r /var/lib/nut";
+  # system.activationScripts.var-lib-nut = "mkdir -p /var/lib/nut; chmod o-r /var/lib/nut";
 
   #services.apcupsd.enable = true;
   #services.prometheus.exporters.apcupsd.enable = true;
@@ -36,7 +36,7 @@
   # };
 
   networking.firewall.allowedTCPPorts = [
-    3493 # nut/upsd
+    # 3493 # nut/upsd
     3100 # loki
     9100 # node exporter
   ];
