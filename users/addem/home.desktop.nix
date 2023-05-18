2ca8d732a7544691e8916492211a48dd0baa07bd
Andreas Mårtensson <andreas@addem.se>
2023-05-18 20:03:59 +0200

    vscode

--- users/addem/home.desktop.nix
+++ users/addem/home.desktop.nix
@@ -11,6 +11,7 @@
     ./sway.nix
     ./ulauncher.nix
     ./variety.nix
+    ./vscode.nix
     ./waybar.nix
   ];
 
@@ -27,33 +28,9 @@
   home.sessionVariables = {
     MOZ_ENABLE_WAYLAND = "1";
     WLR_DRM_NO_MODIFIERS = "1";
+    NIXOS_OZONE_WL = "1";
   };
 
-  programs.vscode = {
-    enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      arcticicestudio.nord-visual-studio-code
-      # brian-anders.sublime-duplicate-text
-      # Cardinal90.multi-cursor-case-preserve
-      denoland.vscode-deno
-      esbenp.prettier-vscode
-      firefox-devtools.vscode-firefox-debug
-      golang.go
-      # jallen7usa.vscode-cue-fmt
-      jnoortheen.nix-ide
-      matklad.rust-analyzer
-      # maximus136.change-string-case
-      # ms-python.isort
-      ms-python.python
-      ms-python.vscode-pylance
-      ms-vscode.cpptools
-      ms-vscode.makefile-tools
-      # nico-castell.linux-desktop-file
-      redhat.vscode-yaml
-      tomoki1207.pdf
-      # xoronic.pestfile
-    ];
-  };
 
   services.batsignal.enable = true; # todo: auto suspend
   # services.poweralertd.enable = true;
---
+++ users/addem/vscode.nix
@@ -0,0 +1,28 @@
+{ pkgs, ...}:
+{
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      arcticicestudio.nord-visual-studio-code
+      # brian-anders.sublime-duplicate-text
+      # Cardinal90.multi-cursor-case-preserve
+      denoland.vscode-deno
+      esbenp.prettier-vscode
+      firefox-devtools.vscode-firefox-debug
+      golang.go
+      # jallen7usa.vscode-cue-fmt
+      jnoortheen.nix-ide
+      matklad.rust-analyzer
+      # maximus136.change-string-case
+      # ms-python.isort
+      ms-python.python
+      ms-python.vscode-pylance
+      ms-vscode.cpptools
+      ms-vscode.makefile-tools
+      # nico-castell.linux-desktop-file
+      redhat.vscode-yaml
+      tomoki1207.pdf
+      # xoronic.pestfile
+    ];
+  };
+}
